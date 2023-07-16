bbd682156a9d82e43222b5d5137630079fbbb8a2
Harsh Singh Chauhan <hchouhan689@gmail.com>
2023-07-16 10:23:28 +0530

    Update Todo.js

--- src/componente/Todo.js
+++ src/componente/Todo.js
@@ -4,11 +4,13 @@ import {AiFillDelete} from 'react-icons/ai';
 
 function Todo({text,updateMode,deleteMode}) {
   return (
-    <div className='todo'>
+    <div className='card todo'>
+      <div className='newcard'>
         <div className='text'> {text}</div>
         <div className="icons">
             <BiEdit className="icon" onClick={updateMode}/>
             <AiFillDelete  className="icon" onClick={deleteMode}/>
+        </div>
         </div>
       
     </div>
